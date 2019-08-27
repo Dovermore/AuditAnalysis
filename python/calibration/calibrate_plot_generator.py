00bbced9635f45a4d91a4ef96b23820896219eb5
Dovermore <calvinyeye0627@gmail.com>
2019-08-28 00:31:21 +1000

    add plots and some other data

--- python/calibration/calibrate_plot_generator.py
+++ python/calibration/calibrate_plot_generator.py
@@ -22,6 +22,8 @@ def generate_plot(all_data_path):
             all_data[data_type] = pd.concat([all_data[data_type], data])
 
     for data_type in all_data:
+        if data_type in ["pdf", "cdf"]:
+            continue
         print(data_type)
         all_data[data_type] = all_data[data_type].set_index("legend")
         data = all_data[data_type]
@@ -34,14 +36,25 @@ def generate_plot(all_data_path):
         plt.xticks(data.columns)
         plt.legend(data.index)
         plt.title(data_type)
+        fig_path = path.join("figures", path.basename(all_data_path))
+        if not path.exists(fig_path):
+            os.makedirs(fig_path)
+        plt.savefig(path.join(fig_path, data_type.replace(".", "_")+".png"))
         plt.show()
 
 
 # scratch tests
 if __name__ == "__main__":
-    n = 20000
-    m = 1000
-    replacement = True
-    step = 1
-    all_data_path = path.join("calibrated_data", f"election_n={n:06d}_m={m:05d}_replacement={replacement}_step={step}")
-    generate_plot(all_data_path)
+    base_path = path.abspath("calibrated_data")
+    for data_path in os.listdir(base_path):
+        data_path = path.join(base_path, data_path)
+        if not path.isdir(data_path):
+            continue
+        generate_plot(data_path)
+
+    # n = 20000
+    # m = 1000
+    # replacement = True
+    # step = 1
+    # all_data_path = path.join("calibrated_data", f"election_n={n:06d}_m={m:05d}_replacement={replacement}_step={step}")
+    # generate_plot(all_data_path)
